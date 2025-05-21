be4ef0863cd3a88ec621f6d33e417331bb0f8148
Enes Akdemir <1enesakdemir@gmail.com>
2025-05-21 09:51:58 +0300

    fix google seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,7 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <meta name="google-site-verification" content="t4YRgCNRtrE29B-i8l4MEOa-QYswVYGtRs3TiLjD0oQ" />
       </head>
       <body>
         {children}
